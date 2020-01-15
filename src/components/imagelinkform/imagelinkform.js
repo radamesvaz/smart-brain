952f8ed3ff84e75c6b873e26acac0de585f0b9fe
Radames Vaz <rgvtovar@hotmail.com>
2020-01-15 12:18:58 -0400

    placeholder

--- src/components/imagelinkform/imagelinkform.js
+++ src/components/imagelinkform/imagelinkform.js
@@ -6,11 +6,11 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) =>{
     return(
         <div>
         <p className='f3'>
-            {'This App will detect faces in your pictures. Give it a try!'}
+            {'This App will detect a face when you give it a picture. Go ahead and try!'}
         </p>
             <div className='center'>
                 <div className='form pa4 br3 shadow-3'>
-                 <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                 <input className='f4 pa2 w-70 center' type='text' placeholder='Insert image URL here' onChange={onInputChange}/>
                  <button 
                     onClick={onButtonSubmit}
                     className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue'>Detect</button>
